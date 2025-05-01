97ee0016ec5d936d70278e9739212457ac44964a
PuiiNuii <hengthaingoun7@gmail.com>
2025-05-01 17:08:05 +0700

    update

--- lib/Widgets/banner.dart
+++ lib/Widgets/banner.dart
@@ -1,36 +1,151 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_application_a3/Utils/color.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_application_a3/Utils/color.dart';
 
-class MyBanner extends StatelessWidget {
+// class MyBanner extends StatelessWidget {
+//   const MyBanner({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     Size size = MediaQuery.of(context).size;
+//     return Container(
+//       height: size.height * 0.25,
+//       width: size.width,
+//       color: bannerColor,
+//       child: Padding(
+//         padding: const EdgeInsets.only(left: 25),
+//         child: Stack(
+//           children: [
+//             Column(
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [],
+//             ),
+//             Align(
+//               alignment: Alignment.bottomRight,
+//               child: Image.asset(
+//                 "assets/banner.png",
+//                 height: size.height * 0.25,
+//                 width: size.width,
+//                 fit: BoxFit.cover,
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
+
+import 'package:flutter/material.dart';
+
+class MyBanner extends StatefulWidget {
   const MyBanner({super.key});
 
   @override
+  State<MyBanner> createState() => _MyBannerState();
+}
+
+class _MyBannerState extends State<MyBanner> {
+  final PageController _pageController = PageController();
+  int _currentPage = 0;
+  final List<String> bannerImages = [
+    "assets/banner.png",
+    "assets/banner.png",
+    "assets/banner.png",
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _startAutoScroll();
+  }
+
+  void _startAutoScroll() {
+    Future.delayed(const Duration(seconds: 3), () {
+      if (_pageController.hasClients) {
+        if (_currentPage < bannerImages.length - 1) {
+          _pageController.nextPage(
+            duration: const Duration(milliseconds: 500),
+            curve: Curves.easeInOut,
+          );
+        } else {
+          _pageController.animateToPage(
+            0,
+            duration: const Duration(milliseconds: 500),
+            curve: Curves.easeInOut,
+          );
+        }
+        _startAutoScroll();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      height: size.height * 0.25,
-      width: size.width,
-      color: bannerColor,
-      child: Padding(
-        padding: const EdgeInsets.only(left: 25),
-        child: Stack(
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [],
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.25,
+      child: Stack(
+        children: [
+          // Main banner with padding
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: PageView.builder(
+              controller: _pageController,
+              onPageChanged: (index) {
+                setState(() {
+                  _currentPage = index;
+                });
+              },
+              itemCount: bannerImages.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: Image.asset(
+                      bannerImages[index],
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                );
+              },
             ),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: Image.asset(
-                "assets/banner.png",
-                height: size.height * 0.25,
-                width: size.width,
-                fit: BoxFit.cover,
+          ),
+
+          // Page indicators
+          Positioned(
+            bottom: 12,
+            left: 0,
+            right: 0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(
+                bannerImages.length,
+                (index) => AnimatedContainer(
+                  duration: const Duration(milliseconds: 300),
+                  margin: const EdgeInsets.symmetric(horizontal: 4),
+                  width: _currentPage == index ? 12 : 8,
+                  height: 8,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: _currentPage == index
+                        ? Colors.white
+                        : Colors.white.withOpacity(0.5),
+                  ),
+                ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
